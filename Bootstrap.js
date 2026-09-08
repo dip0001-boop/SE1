23047cfc3ac437e47a3c2c477a57af0b64243042
dip0001-boop <dip0001@tarneitcollege.vic.edu.au>
2026-09-08 10:45:11 +1000

    Update Bootstrap.js

--- Bootstrap.js
+++ Bootstrap.js
@@ -1,1 +1,9 @@
+import { Engine } from "./Engine.js";
 
+export function bootstrap() {
+  const canvas = document.getElementById("viewport");
+  if (!canvas) throw new Error("Canvas #viewport not found");
+  const engine = new Engine(canvas);
+  engine.start();
+  return engine;
+}
