e65542e741607e6f75681b453d7bee64a4b96c23
dip0001-boop <dip0001@tarneitcollege.vic.edu.au>
2026-09-08 10:41:10 +1000

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
+import express from "express";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+import { galaxyRouter } from "./galaxyRoutes.js";
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/galaxy", galaxyRouter);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(__dirname));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log("Singularity Engine 1 backend running on port " + PORT);
+});
