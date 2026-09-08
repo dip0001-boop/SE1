120e10c692b7ce3530e6fde57d5df2dcfe76aacf
dip0001-boop <dip0001@tarneitcollege.vic.edu.au>
2026-09-08 10:46:44 +1000

    Update Entity.js

--- Entity.js
+++ Entity.js
@@ -1,1 +1,11 @@
+let NEXT_ENTITY_ID = 1;
 
+export function createEntityId() {
+  return NEXT_ENTITY_ID++;
+}
+
+export function resetEntityIdCounter() {
+  NEXT_ENTITY_ID = 1;
+}
+
+export const INVALID_ENTITY = 0;
