b17e44f6b658a1622f9825fc1761bf27c70d279d
dip0001-boop <dip0001@tarneitcollege.vic.edu.au>
2026-09-08 10:45:23 +1000

    Update Engine.js

--- Engine.js
+++ Engine.js
@@ -1,1 +1,36 @@
+import { Loop } from "./Loop.js";
+import { Input } from "./Input.js";
+import { ECS } from "./ECS.js";
+import { Renderer } from "./Renderer.js";
+import { GalaxySystem } from "./GalaxySystem.js";
+import { GalaxyManager } from "./GalaxyManager.js";
 
+export class Engine {
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.ecs = new ECS();
+    this.input = new Input(canvas);
+
+    this.galaxySystem = new GalaxySystem(this.ecs);
+    this.galaxyManager = new GalaxyManager(this.ecs, this.galaxySystem);
+
+    this.renderer = new Renderer(canvas, this.ecs);
+    this.loop = new Loop((dt) => this.update(dt), () => this.render());
+  }
+
+  start() {
+    this.loop.start();
+  }
+
+  stop() {
+    this.loop.stop();
+  }
+
+  update(dt) {
+    this.galaxySystem.update(dt);
+  }
+
+  render() {
+    this.renderer.render();
+  }
+}
