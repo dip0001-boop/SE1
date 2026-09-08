57aedbdcb33781841b1e5a4c896dcf0197332033
dip0001-boop <dip0001@tarneitcollege.vic.edu.au>
2026-09-08 10:45:59 +1000

    Update Loop.js

--- Loop.js
+++ Loop.js
@@ -1,1 +1,31 @@
+export class Loop {
+  constructor(update, render) {
+    this.update = update;
+    this.render = render;
+    this.running = false;
+    this.last = 0;
+    this.rafId = 0;
+    this.tick = this.tick.bind(this);
+  }
 
+  start() {
+    if (this.running) return;
+    this.running = true;
+    this.last = performance.now();
+    this.rafId = requestAnimationFrame(this.tick);
+  }
+
+  stop() {
+    this.running = false;
+    cancelAnimationFrame(this.rafId);
+  }
+
+  tick(now) {
+    if (!this.running) return;
+    const dt = Math.min(0.1, (now - this.last) / 1000);
+    this.last = now;
+    this.update(dt);
+    this.render();
+    this.rafId = requestAnimationFrame(this.tick);
+  }
+}
