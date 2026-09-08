b0ed72d63d5913255d2ac6c64960d536c2ad33f5
dip0001-boop <dip0001@tarneitcollege.vic.edu.au>
2026-09-08 10:46:15 +1000

    Implement Input class for keyboard and mouse events

--- Input.js
+++ Input.js
@@ -1,1 +1,18 @@
+export class Input {
+  constructor(canvas) {
+    this.keys = new Set();
+    this.mouse = { x: 0, y: 0, down: false };
 
+    window.addEventListener("keydown", (e) => this.keys.add(e.key));
+    window.addEventListener("keyup", (e) => this.keys.delete(e.key));
+
+    canvas.addEventListener("mousemove", (e) => {
+      const rect = canvas.getBoundingClientRect();
+      this.mouse.x = (e.clientX - rect.left) / rect.width;
+      this.mouse.y = (e.clientY - rect.top) / rect.height;
+    });
+
+    canvas.addEventListener("mousedown", () => (this.mouse.down = true));
+    canvas.addEventListener("mouseup", () => (this.mouse.down = false));
+  }
+}
