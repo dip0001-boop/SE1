77b9b57f65ed389c803d9939285377620640f438
dip0001-boop <dip0001@tarneitcollege.vic.edu.au>
2026-09-08 10:41:30 +1000

    Add galaxyRouter to handle sector requests

--- galaxyRoutes.js
+++ galaxyRoutes.js
@@ -1,1 +1,10 @@
+import { Router } from "express";
+import { getSectorSeed } from "./storage.js";
 
+export const galaxyRouter = Router();
+
+galaxyRouter.get("/sector", (req, res) => {
+  const sectorId = req.query.sectorId || "0,0,0";
+  const seed = getSectorSeed(sectorId);
+  res.json({ sectorId, seed });
+});
